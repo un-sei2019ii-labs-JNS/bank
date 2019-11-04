f48af2feed46099e98e16571010ad3e513685573
jreinaz <jreinaz@unal.edu.co>
2019-11-04 14:47:49 -0500

    Reenvio de clase Cuenta y CuentaDao

--- mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/dao/CuentaDao.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/dao/CuentaDao.java
@@ -1,4 +1,28 @@
 package co.edu.unal.se1.dataAccess.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+import co.edu.unal.se1.dataAccess.model.Cuenta;
+
+@Dao
 public interface CuentaDao {
+    @Query("SELECT * FROM cuenta")
+    List<Cuenta> getAllUsers();
+
+    @Query("SELECT * FROM cuenta WHERE cuentaid = :id")
+    Cuenta getCuentaById(int id);
+
+    @Insert
+    void createCuenta(Cuenta cuenta);
+
+    @Update
+    void updateCuenta(Cuenta cuenta);
+
+    @Delete
+    void deleteCuenta(Cuenta cuenta);
 }
--- mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/db/Database.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/db/Database.java
@@ -3,10 +3,13 @@ package co.edu.unal.se1.dataAccess.db;
 import androidx.room.RoomDatabase;
 
 import co.edu.unal.se1.dataAccess.dao.UserDao;
+import co.edu.unal.se1.dataAccess.model.Cuenta;
 import co.edu.unal.se1.dataAccess.model.User;
 
-@androidx.room.Database(entities = {User.class}, version = 1)
+@androidx.room.Database(entities = {User.class,Cuenta.class}, version = 1)
 public abstract class Database extends RoomDatabase {
 
     public abstract UserDao userDao();
+    //public abstract CuentaDao cuentaDao();
+
 }
